19ed08e90de43881e3637815e063fbf780210938
Patryk94 <pat_rik94@hotmail.com>
2025-06-06 07:20:01 +0200

    Auto update: 2025-06-06 07:20:01

--- bash-exercies/permissions.sh
+++ bash-exercies/permissions.sh
@@ -5,6 +5,19 @@ GROUP=DEV
 FILE=/tmp/output.txt
 
 if [[ -z $USER || -z $GROUP ]]; then
-  echo '$USER and $GROUP are empty'
+  echo 'USER and GROUP are empty'
   exit 11
 fi
+
+if [[ -f $FILE ]]; then
+  echo "Changing permissions for FILE"
+  chown $USER:$GROUP $FILE
+elif [[ -d $FILE ]]; then
+  echo "Changing permissions to Directory"
+  chown -R $USER:$GROUP $FILE
+else
+  echo "$FILE is not proper file"
+  exit 12
+fi
+
+exit 0
